a2e6bc1430b95b1870d9b6bc2c4a0c60c2c3bacf
TSvetkov_Aleksei <vilegu@yandex.ru>
2024-04-05 06:14:21 +0300

    Cosmetic changes 1

--- demo/src/main/java/com/example/demo/App.java
+++ demo/src/main/java/com/example/demo/App.java
@@ -19,33 +19,29 @@ import javafx.scene.control.Alert.AlertType;
 
 public class App extends Application {
 
-    public double sqrt(double x) {
-        return Math.pow(x, 3);
-    }
+    /* Enter the function after the arrow with the argument x[0] */
+    Operation operation = x -> Math.pow(x[0], 2);
 
     double scaleSize = 1;
     int accuracy = 20;
-    int SIZE = 60;
-    final int WIDTH = 1920;
-    final int HEIGHT = 1080;
+    final int SIZE = 60;
+    final int WIDTH = 1080;
+    final int HEIGHT = 720;
     int N;
     String TYPE;
-    final double FROM = -2 * SIZE; 
-    final double TO = 2 * SIZE;
+    final double FROM = -1 * SIZE;
+    final double TO = 1 * SIZE;
 
     public double frac(double x, double fraction) {
-        switch (TYPE) {
-            case "start":
-                return x;
-            case "middle":
-                return x + fraction / 2;
-            case "finish":
-                return x + fraction;
-            default: {
+        return switch (TYPE) {
+            case "start" -> x;
+            case "middle" -> x + fraction / 2;
+            case "finish" -> x + fraction;
+            default -> {
                 Random rnd = new Random();
-                return x + (rnd.nextDouble(fraction));
+                yield x + (rnd.nextDouble(fraction));
             }
-        }
+        };
     }
 
     public int sizeof(int x) {
@@ -56,8 +52,11 @@ public class App extends Application {
         Scanner scanner = new Scanner(System.in);
         N = scanner.nextInt();
         TYPE = scanner.next();
-        Group group = new Group();
 
+        Group group = new Group();
+        Scene scene = new Scene(group, WIDTH, HEIGHT);
+
+        /* Creating a grid */
         for (int i = -WIDTH * SIZE / 2; i < WIDTH * SIZE / 2 - 1; i++) {
             if (i % SIZE == 0) {
                 Line lineX = new Line(i, Integer.MIN_VALUE, i, Integer.MAX_VALUE);
@@ -67,7 +66,6 @@ public class App extends Application {
                 );
             }
         }
-
         for (int i = HEIGHT * SIZE / 2; i > -HEIGHT * SIZE; i--) {
             if (i % SIZE == 0) {
                 Line lineY = new Line(Integer.MIN_VALUE, i, Integer.MAX_VALUE, i);
@@ -78,21 +76,24 @@ public class App extends Application {
             }
         }
 
-        Operation operation = x -> sqrt(x[0]);
-
+        /* Size of a fraction */
         double fraction = ((TO - FROM) / N);
-        double sum = 0;
 
+        double sum = 0;
         for (double i = FROM; i < TO; i += fraction) {
             double c = frac(i, fraction);
+
+            /* Creating rectangles for splitting */
             Rectangle figure = new Rectangle(
                 i + (double) WIDTH / 2, 
                 Math.min(HEIGHT / 2, -operation.execute((double) c / SIZE) * SIZE + (double) HEIGHT / 2),
                 fraction,
                 Math.abs(operation.execute((double) c / SIZE) * SIZE)
             );
+
+            /* Calculating the integral sum */
             sum += (fraction / SIZE) * operation.execute((double) c / SIZE);
-            figure.setFill(Color.AQUAMARINE);
+            figure.setFill(Color.PINK);
             figure.setStroke(Color.BLACK);
             group.getChildren().add(
                     figure
@@ -100,13 +101,13 @@ public class App extends Application {
         }
         System.out.println(sum);
 
+        /* Drawing numbers along the x and y axes */
         for (int i = -WIDTH * SIZE / 2; i < WIDTH * SIZE / 2 - 1; i = i + SIZE) {
             if (-1 <= i / SIZE && i / SIZE <= 1 || (i / SIZE) % 5 == 0) {
                 Text text = new Text((double) WIDTH / 2 + i - (double) sizeof(i) * 2, (double) HEIGHT / 2 + 15, i / SIZE + "");
                 group.getChildren().add(text);
             }
         }
-
         for (int i = HEIGHT * SIZE / 2; i > -HEIGHT * SIZE; i = i - SIZE) {
             if (((-1 <= i / SIZE) && (i / SIZE <= 1)) || ((i / SIZE % 5 == 0) && (i / SIZE != 0))) {
                 if (i / SIZE != 0) {
@@ -119,6 +120,7 @@ public class App extends Application {
             }
         }
 
+        /* Drawing a graph of a given function */
         for (double  i = FROM; i < TO; i++) {
             Line figure = new Line(
                     i + (double) WIDTH / 2,
@@ -132,83 +134,78 @@ public class App extends Application {
             );
         }
 
-        Scene scene = new Scene(group, WIDTH, HEIGHT);
-
-
-        Circle player = new Circle(2, Color.GREEN); // Создание круглого объекта (персонажа)
-        player.setTranslateX((double) WIDTH / 2); // Установка начальной позиции персонажа по горизонтали
-        player.setTranslateY((double) HEIGHT / 2); // Установка начальной позиции персонажа по вертикали
-
+        /* Creating a "character" for interactive */
+        Circle player = new Circle(2, Color.GREEN);
+        player.setTranslateX((double) WIDTH / 2);
+        player.setTranslateY((double) HEIGHT / 2);
         double[] coordX = {0};
         double[] coordY = {0};
-
         group.getChildren().add(player);
-
         group.getChildren().addAll(
                 new Line(Integer.MIN_VALUE, (double) HEIGHT / 2, Integer.MAX_VALUE, (double) HEIGHT / 2),
                 new Line((double) WIDTH / 2, Integer.MIN_VALUE, (double) WIDTH / 2, Integer.MAX_VALUE)
         );
 
-
+        /* Interactive */
+        double acs = (double) accuracy / SIZE;
         scene.setOnKeyPressed(event -> {
                     KeyCode code = event.getCode();
-
                     switch (code) {
+                        /* The movement of the "character" and the screen */
                         case UP:
-                            group.setTranslateY(group.getTranslateY() + accuracy); // Движение вверх
+                            group.setTranslateY(group.getTranslateY() + accuracy);
                             player.setTranslateY(player.getTranslateY() - accuracy);
-                            coordY[0] = coordY[0] + (double) accuracy / SIZE;
+                            coordY[0] = coordY[0] + acs;
                             break;
                         case DOWN:
-                            group.setTranslateY(group.getTranslateY() - accuracy); // Движение вниз
+                            group.setTranslateY(group.getTranslateY() - accuracy);
                             player.setTranslateY(player.getTranslateY() + accuracy);
-                            coordY[0] = coordY[0] - (double) accuracy / SIZE;
+                            coordY[0] = coordY[0] - acs;
                             break;
                         case LEFT:
-                            group.setTranslateX(group.getTranslateX() + accuracy); // Движение влево
+                            group.setTranslateX(group.getTranslateX() + accuracy);
                             player.setTranslateX(player.getTranslateX() - accuracy);
-                            coordX[0] = coordX[0] - (double) accuracy / SIZE;
+                            coordX[0] = coordX[0] - acs;
                             break;
                         case RIGHT:
-                            group.setTranslateX(group.getTranslateX() - accuracy); // Движение вправо
+                            group.setTranslateX(group.getTranslateX() - accuracy);
                             player.setTranslateX(player.getTranslateX() + accuracy);
-                            coordX[0] = coordX[0] + (double) accuracy / SIZE;
+                            coordX[0] = coordX[0] + acs;
                             break;
 
-
+                        /* The movement of the "character" */
                         case W:
                             player.setTranslateY(player.getTranslateY() - accuracy);
-                            coordY[0] = coordY[0] + (double) accuracy / SIZE;
+                            coordY[0] = coordY[0] + acs;
                             break;
                         case S:
                             player.setTranslateY(player.getTranslateY() + accuracy);
-                            coordY[0] = coordY[0] - (double) accuracy / SIZE;
+                            coordY[0] = coordY[0] - acs;
                             break;
                         case A:
                             player.setTranslateX(player.getTranslateX() - accuracy);
-                            coordX[0] = coordX[0] - (double) accuracy / SIZE;
+                            coordX[0] = coordX[0] - acs;
                             break;
                         case D:
                             player.setTranslateX(player.getTranslateX() + accuracy);
-                            coordX[0] = coordX[0] + (double) accuracy / SIZE;
+                            coordX[0] = coordX[0] + acs;
                             break;
 
+                        /* Showing the coordinates of the "character" */
                         case H:
                             Dialog<String> dialog = new Dialog<>();
                             dialog.setTitle("Player Coordinates");
                             dialog.setHeaderText("Current Coordinates: (" + coordX[0] + ", " + coordY[0] + ")");
-
                             dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
-
                             dialog.showAndWait();
                             break;
 
+                        /* Changing the accuracy of the movement of the "character" */
                         case C:
                             TextInputDialog dialogue = new TextInputDialog(Double.toString((double) accuracy / SIZE));
                             dialogue.setTitle("Set Accuracy");
                             dialogue.setHeaderText("Enter the new accuracy value:");
                             dialogue.showAndWait().ifPresent(newValue -> {
-                                // Проверка и присвоение нового значения accuracyValue
                                 try {
                                     accuracy = (int) (Double.parseDouble(newValue) * SIZE);
                                 } catch (NumberFormatException e) {
@@ -219,6 +216,8 @@ public class App extends Application {
                                 }
                             });
                             break;
+
+                        /* The approach/distance */
                         case X:
                             Dialog dialog1 = new Dialog();
                             dialog1.setTitle("Set scale");
@@ -243,6 +242,7 @@ public class App extends Application {
                 }
         );
         primaryStage.setScene(scene);
+        primaryStage.setTitle("function: x^2 "  + "[" + FROM / SIZE + "; " +  TO / SIZE + "]  integral sum : " + sum + " number of splits : " + N);
         primaryStage.show();
 
     }
